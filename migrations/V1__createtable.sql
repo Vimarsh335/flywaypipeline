1b888a5a5e200ccc168adaa1668a93a2f80cab81
vimarsh12 <vimarshgupta12@gmail.com>
2026-01-13 02:04:56 +0530

    create table

--- migrations/V1__createtable.sql
+++ migrations/V1__createtable.sql
@@ -3,5 +3,5 @@ CREATE TABLE public.profile (
   user_id uuid REFERENCES auth.users NOT NULL PRIMARY KEY,
   username TEXT NULL,
   email TEXT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
