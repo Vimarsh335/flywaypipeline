188817f71be4ebebd9f4922dbbdd03a5793c47bd
vimarsh12 <vimarshgupta12@gmail.com>
2026-01-13 02:00:09 +0530

    create table

--- schemas/V1__createtable.sql
+++ schemas/V1__createtable.sql
@@ -2,5 +2,6 @@
 CREATE TABLE public.profile (
   user_id uuid REFERENCES auth.users NOT NULL PRIMARY KEY,
   username TEXT NULL,
-  email TEXT NULL
+  email TEXT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
