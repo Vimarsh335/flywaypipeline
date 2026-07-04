0ce2da8d3273d6faf0551d65d9eeb8fcc52556d9
vimarsh12 <vimarshgupta12@gmail.com>
2026-07-05 01:50:15 +0530

    this is v1 tag

--- database/repeatables/R__get_employee_by_user_id.sql
+++ database/repeatables/R__get_employee_by_user_id.sql
@@ -1,16 +1,10 @@
 CREATE OR REPLACE FUNCTION public.get_employee_by_user_id(
     p_user_id UUID
 )
-RETURNS TABLE (
-    user_id UUID,
-    username TEXT,
-    email TEXT,
-    created_at TIMESTAMPTZ
-)
+RETURNS void
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    RETURN QUERY
-   RAISE NOTICE 'Fetching employee with user_id: %', p_user_id;
+    RAISE NOTICE 'Fetching employee with user_id: %', p_user_id;
 END;
 $$;
