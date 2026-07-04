9ef8a4169798896602b3d05727ecf4cca3c0069b
vimarsh12 <vimarshgupta12@gmail.com>
2026-07-04 22:26:34 +0530

    changed theyml

---
+++ migrations/V3__createstd.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE public.std (
+  user_id uuid REFERENCES auth.users NOT NULL PRIMARY KEY,
+  username TEXT NULL,
+  email TEXT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
