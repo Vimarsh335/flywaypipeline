e4ea5c8306d168fb49c65ef0c59b2b917c6ddeaf
vimarsh.gupta@arrise.com <vimarsh.gupta@arrise.com>
2026-07-05 21:38:33 +0530

    V8 changes

--- database/undo/U9__demo4.sql
+++ database/undo/U9__demo4.sql
@@ -1,6 +1,1 @@
-CREATE TABLE IF NOT EXISTS public.demo4 (
-  user_id uuid REFERENCES auth.users NOT NULL PRIMARY KEY,
-  username TEXT NULL,
-  email TEXT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+DROP TABLE IF EXISTS public.demo4;
